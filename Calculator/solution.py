5bf93189b151857c61114bf5e2a793400e12348e
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-01-24 11:31:59 +0500

    Update solution.py

--- Calculator/solution.py
+++ Calculator/solution.py
@@ -1,16 +1,36 @@
 # Simple Calculator in Python
 
+# Adding the two values
 def add(x, y):
   return x + y
 
+# Subtracting the two values
 def subtract(x, y):
   return x - y
 
+# Multiplying the two values
 def multiply(x, y):
   return x * y
 
+# Dividing the two values
 def divide(x, y):
   return x // y
 
+# Modulus of two values
 def modulo(x, y):
   return x % y
+
+# start function to begin with the calculations
+def start():
+  a = int(input("Enter the value of a: "))
+  b = int(input("Enter the value of b: "))
+  print("Value of a is: ", a)
+  print("Value of b is: ", b)
+  print("The sum of a and b is: ", add(a, b))
+  print("The difference of a and b is: ", subtract(a, b))
+  print("The product of a and b is: ", multiply(a, b))
+  print("The dividend of a and b is: ", divide(a, b))
+  print("The modulus of a and b is: ", modulo(a, b))
+
+start()
+
