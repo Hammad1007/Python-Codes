d150efe5c2d95ea1f54f263e3b6d49f7c9ea7e8e
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-01-27 12:18:48 +0500

    Update solution.py

--- Quadratic Solution/solution.py
+++ Quadratic Solution/solution.py
@@ -1,2 +1,13 @@
 # Program to find the quadratic roots
 import cmath
+
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = int(input("Enter the value of c: "))
+print(f"The values of a, b and c are {a}, {b} and {c} respectively.")
+
+disc = b**2 - 4 * a * c
+root1 = (-b - cmath.sqrt(disc))/(2 * a)
+root2 = (-b + cmath.sqrt(disc))/(2 * a)
+
+print(f"The roots are {root1} and {root2}.")
