ce9f6930219a4d1c4d87a335bd2e9e5fe2bd39ef
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-02-19 22:31:05 +0500

    Update solution.py

--- Inheritance/Classes/solution.py
+++ Inheritance/Classes/solution.py
@@ -8,8 +8,8 @@ class B:
 
 # An object of class A calling the variable of class A
 x = A()
-print(x.a)
+print(x.a)  # prints 10
 
 # An object of class B calling the variable of class B
 y = B()
-print(y.b)
+print(y.b)    # prints 20
