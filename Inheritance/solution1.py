4f74ab18dc4c3b27cba66f0b2b9d880a37cf4a93
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-02-24 10:06:03 +0500

    Update solution1.py

--- Inheritance/solution1.py
+++ Inheritance/solution1.py
@@ -1,1 +1,10 @@
 # Code here
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name;
+    self.age = age
+    
+  def printdet(self):
+    print("Name is: ", self.name)
+    print("Age is: ", self.age)
