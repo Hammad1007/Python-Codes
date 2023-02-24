6bef559f9f87fa868f815c68a25409e769285b73
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-02-24 10:08:57 +0500

    Update solution1.py

--- Inheritance/solution1.py
+++ Inheritance/solution1.py
@@ -1,5 +1,6 @@
 # Code here
 
+# Parent class is Person class
 class Person:
   def __init__(self, name, age):
     self.name = name;
@@ -8,3 +9,11 @@ class Person:
   def printdet(self):
     print("Name is: ", self.name)
     print("Age is: ", self.age)
+
+class Student(Person):
+  def __init__(self, name, age, gender, degree):
+    super().__init__(name, age)
+    self.gender = gender
+    self.degree = degree
+    
+    
