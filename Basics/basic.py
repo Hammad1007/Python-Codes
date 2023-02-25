e8b9c830eb8916ac03280ee534a7da1220fe9486
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-02-25 11:58:53 +0500

    Update basic.py

--- Basics/basic.py
+++ Basics/basic.py
@@ -1,1 +1,14 @@
 # Code here
+
+# First initialize and declare a variable
+
+a = 9               # integer type
+b = 9.8             # float type
+c = 9.888888888888  # double type
+d = 'a'             # character type
+
+# Print the variable value through print statements
+print(a)
+print(b)
+print(c)
+print(d)
