1b8c11b2b438ddabcfd1bd37632afb42b653e83f
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-03-02 11:33:45 +0500

    Update intro.py

--- Faker/intro.py
+++ Faker/intro.py
@@ -1,19 +1,1 @@
-# Code here
 
-# !pip install faker
-
-# from the library faker, import Faker the class
-from faker import Faker
-
-# make a fake object of class Faker
-fake = Faker()
-
-# create a fake name
-name = fake.name()
-
-# create a fake address
-address = fake.address()
-
-# print statements
-print(name)
-print(address)
